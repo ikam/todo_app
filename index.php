088d935cc6e7fac567774fa80ac77e7ceca88cf2
ikam <ikam@dedikam.com>
2022-05-24 10:49:48 +0200

    Affichage des articles sur la page d'accueil.

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+    $category = array(fn($a) => $a);
+}
+
 
 ?>
 
@@ -9,17 +17,20 @@
     <link rel="stylesheet" href="public/css/index.css">
     <title>BLOG APP</title>
 </head>
-
 <body>
-
 <div class="container">
     <?php require_once 'includes/header.php' ?>
     <div class="content">
-
+        <div class="articles-container">
+            <?php foreach ($articles as $article) : ?>
+                <div class="article block">
+                    <div class="img-container" style="background-image: url(<?= $article['image'] ?>);"></div>
+                    <h2><?= $article['title'] ?></h2>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
     <?php require_once 'includes/footer.php' ?>
 </div>
-
 </body>
-
 </html>
