8db3a11e2d39b6cd37ec3ddf2c955da559842a0c
ikam <ikam@dedikam.com>
2022-05-25 11:34:06 +0200

    Affichage d'un article

--- index.php
+++ index.php
@@ -42,22 +42,23 @@ if (file_exists($filename)) {
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
+
 <head>
     <?php require_once 'includes/head.php' ?>
     <link rel="stylesheet" href="public/css/index.css">
-    <title>Blog APP</title>
+    <title>Blog PHP</title>
 </head>
+
 <body>
 <div class="container">
     <?php require_once 'includes/header.php' ?>
     <div class="content">
-
         <div class="newsfeed-container">
             <ul class="category-container">
                 <li class="<?= $selectedCat ? '' : 'cat-active' ?>">
                     <a href="/">
-                        Tous les articles <span class="small">(<?= count($articles) ?>)</span>
+                        Tous les articles<span class="small">(<?= count($articles) ?>)</span>
                     </a>
                 </li>
                 <?php foreach ($categories as $catName => $catNum) : ?>
@@ -74,13 +75,13 @@ if (file_exists($filename)) {
                         <h2 class="p-10"><?= $cat ?></h2>
                         <div class="articles-container">
                             <?php foreach ($articlesPerCategories[$cat] as $article) : ?>
-                                <div class="article block">
+                                <a href="/show-article.php?id=<?= $article['id'] ?>" class="article block">
                                     <div class="overflow">
                                         <div class="img-container"
                                              style="background-image: url(<?= $article['image'] ?>);"></div>
                                     </div>
                                     <h3><?= $article['title'] ?></h3>
-                                </div>
+                                </a>
                             <?php endforeach; ?>
                         </div>
                     <?php endforeach; ?>
@@ -88,21 +89,21 @@ if (file_exists($filename)) {
                     <h2><?= $selectedCat ?></h2>
                     <div class="articles-container">
                         <?php foreach ($articlesPerCategories[$selectedCat] as $article) : ?>
-                            <div class="article block">
+                            <a href="/show-article.php?id=<?= $article['id'] ?>" class="article block">
                                 <div class="overflow">
                                     <div class="img-container"
                                          style="background-image: url(<?= $article['image'] ?>);"></div>
                                 </div>
                                 <h3><?= $article['title'] ?></h3>
-                            </div>
+                            </a>
                         <?php endforeach; ?>
                     </div>
                 <?php endif; ?>
-                <?php ?>
             </div>
         </div>
     </div>
     <?php require_once 'includes/footer.php' ?>
 </div>
 </body>
+
 </html>
