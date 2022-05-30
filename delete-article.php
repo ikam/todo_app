50f2e82153d5792308bd6ba8de9593bba0392a3f
ikam <ikam@dedikam.com>
2022-05-30 11:40:59 +0200

    Ajout du bouton DELETE

--- delete-article.php
+++ delete-article.php
@@ -8,12 +8,10 @@ $id = $_GET['id'] ?? '';
 
 if (!$id) {
     header('Location: /');
-} else {
-    if (file_exists($filename)) {
-        $articles = json_decode(file_get_contents($filename), true) ?? [];
-        $articleIdx = array_search($id, array_column($articles, 'id'));
-        array_splice($articles, $articleIdx, 1);
-        file_put_contents($filename, json_encode($articles));
-        header('Location: /');
-    }
+} else if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+    $articleIdx = array_search($id, array_column($articles, 'id'));
+    array_splice($articles, $articleIdx, 1);
+    file_put_contents($filename, json_encode($articles));
+    header('Location: /');
 }
