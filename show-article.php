124e27cded20b7e20b523b7efaaeb6ccb4a1c6ab
ikam <ikam@dedikam.com>
2022-05-30 11:30:53 +0200

    Ajout du bouton SUPPRESS

--- show-article.php
+++ show-article.php
@@ -15,25 +15,27 @@ if (!$id) {
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <?php require_once 'includes/head.php' ?>
     <link rel="stylesheet" href="public/css/show-article.css">
-    <title>Article</title>
+    <title>Document</title>
 </head>
 <body>
 <div class="container">
     <?php require_once 'includes/header.php' ?>
     <div class="content">
         <div class="article-container">
-            <a href="/" class="article-back">< Retour à la liste des articles</a>
+            <a href="/" class="article-back">Retour à la liste des articles</a>
             <div class="article-cover-img" style="background-image: url(<?= $article['image'] ?>);"></div>
             <h1 class="article-title"><?= $article['title'] ?></h1>
             <div class="separator"></div>
             <p class="article-content"><?= $article['content'] ?></p>
             <div class="action">
-                <a class="btn btn-primary" href="/form-article.php?id=<?= $article['id'] ?>">Editer l'article</a>
+                <a class="btn btn-danger" href="/delete-article.php?id=<?= $article['id'] ?>">Supprimer</a>
+                <a class="btn btn-primary" href="/form-article.php?id=<?= $article['id'] ?>">Modifier l'article</a>
             </div>
         </div>
     </div>
